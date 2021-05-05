15461672bd01cdcf4ba66e4cd7ff1a4492a4b464
the-pro <rockingayas13@gmail.com>
2021-05-05 14:39:42 +0530

    Added data collector for npm

--- extractors/collect.js
+++ extractors/collect.js
@@ -1,5 +1,6 @@
 const docker = require('./docker')
 const github = require('./github')
+const npm = require('./npm')
 
 
 docker.collect().then(
@@ -10,5 +11,10 @@ docker.collect().then(
 github.collect().then(
     () => {
         console.log(`Sucessfully collected github stats for ${Date(Date.now())}`)
+    }
+)
+npm.collectData().then(
+    () => {
+        console.log(`Sucessfully collected npm stats for ${Date(Date.now())}`)
     }
 )
--- extractors/npm.js
+++ extractors/npm.js
@@ -1,26 +1,50 @@
 const fetch = require('node-fetch')
+const fs = require('fs')
 const url = 'https://api.npmjs.org/downloads/point/'
 const packageName = 'juice-shop-ctf-cli'
 
-const extractStats = async (startDate,endDate) => {
-    const downloads = []
-    const sd = new Date(startDate)
-    const ed = new Date(endDate)
-    const dates = []
-    for(let d = sd;d<= ed;d.setDate(d.getDate()+1)){
-        dates.push(`${d.toISOString().split('T')[0]}`)
-    }
+const collectData = async () => {
+    let downloads = fs.readFileSync('statsData/npm.json')
+    downloads = JSON.parse(downloads)
+    let date = new Date(Date.now())
+    date = date.toISOString().split('T')[0]
+    let dates = [date]
     for(const date of dates){
         let ExtractUrl = `${url}${date}:${date}/${packageName}`
             await fetch(ExtractUrl).then(
                 data => data .json()
             ).then(
                 (data) => {
-                    downloads.push([date,data.downloads])
+                    let there = false
+                    for(let i=0;i<downloads.length;i++){
+                        if(downloads[i][0] === date){
+                            there = true
+                        }
+                    }
+                    if(!there){
+                        downloads.push([date,data.downloads])
+                    }
                 }
             )
     }
-    return downloads
+    downloads = JSON.stringify(downloads)
+    fs.writeFileSync('statsData/npm.json',downloads)
 }
 
-module.exports = extractStats
+const getStats = () => {
+    // const sd = new Date(startDate)
+    // const ed = new Date(endDate)
+    // const dates = []
+    // for(let d = sd;d<= ed;d.setDate(d.getDate()+1)){
+    //     dates.push(`${d.toISOString().split('T')[0]}`)
+    // }
+
+    let data = fs.readFileSync('statsData/npm.json')
+    data = JSON.parse(data)
+
+    return data
+}
+
+
+module.exports.getStats = getStats
+module.exports.collectData = collectData
