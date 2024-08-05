f10dcd8f2fcd349401ed4f9d137ee56d0d680091
Bjoern Kimminich <bjoern.kimminich@kuehne-nagel.com>
2024-08-05 16:47:25 +0200

    Add v17.x to statistics

--- extractors/github.js
+++ extractors/github.js
@@ -58,12 +58,12 @@ const fetchData = () => {
 
   const data = []
   const dates = Object.getOwnPropertyNames(githubDataJs)
-  const releases = ['v9', 'v10', 'v11', 'v12', 'v13', 'v14', 'v15', 'v16']
+  const releases = ['v9', 'v10', 'v11', 'v12', 'v13', 'v14', 'v15', 'v16', 'v17']
   data.push(releases)
 
   for (const date of dates) {
     const downloadsPerReleaseByDay = [date]
-    const downloadsPerRelease = { v9: 0, v10: 0, v11: 0, v12: 0, v13: 0, v14: 0, v15: 0, v16: 0 }
+    const downloadsPerRelease = { v9: 0, v10: 0, v11: 0, v12: 0, v13: 0, v14: 0, v15: 0, v16: 0, v17: 0 }
     for (const release of releases) {
       for (const data of githubDataJs[date]) {
         if (data[0].startsWith(release)) {
